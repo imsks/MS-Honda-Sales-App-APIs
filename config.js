27be89091eada0bdb4650328e7e2ad8c626bc32a
sachin <sachinkshuklaoo7@gamil.com>
2020-11-26 13:55:43 +0530

    make it better

--- config.js
+++ config.js
@@ -16,14 +16,14 @@ const dev = {
 };
 // For Production
 const prod = {
-  PORT: "",
-  FIREBASE_API_KEY: "",
-  FIREBASE_AUTH_DOMAIN: "",
-  FIREBASE_DATABASE_URL: "",
-  FIREBASE_PROJECT_ID: "",
-  FIREBASE_STORAGE_BUCKET: "",
-  FIREBASE_MESSAGING_SENDER_ID: "",
-  FIREBASE_APP_ID: "",
+  PORT: 8000,
+  FIREBASE_API_KEY: "AIzaSyD6CHcHCmaUU1vTqxp9GPey08zkqVMxe4k",
+  FIREBASE_AUTH_DOMAIN: "mshondasalesapp.firebaseapp.com",
+  FIREBASE_DATABASE_URL: "https://mshondasalesapp.firebaseio.com",
+  FIREBASE_PROJECT_ID: "mshondasalesapp",
+  FIREBASE_STORAGE_BUCKET: "mshondasalesapp.appspot.com",
+  FIREBASE_MESSAGING_SENDER_ID: "439950269163",
+  FIREBASE_APP_ID: "1:439950269163:web:a79ad2f307ea82308c3cae",
 };
 
 const env = envName === "prod" ? prod : dev;
