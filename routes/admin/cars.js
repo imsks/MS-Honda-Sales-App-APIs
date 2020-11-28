24be3239339bd1ebadddb0b9c1c8a1878905d02c
Sachin <sachinkshuklaoo7@gmail.com>
2020-11-28 09:44:53 +0530

    fixed issues

--- routes/admin/cars.js
+++ routes/admin/cars.js
@@ -4,6 +4,7 @@ const carsController = require("../../controllers/admin/cars");
 // For Auth
 router.route("/add-car-data").post(carsController.addCarData);
 router.route("/get-car-data/:carName/:carModel/:carType").get(carsController.getACarData);
+router.route("/get-car-data-by-id/:id").get(carsController.getACarDataById);
 router.route("/get-all-cars").get(carsController.getAllCarsData);
 router.route("/edit-car-data").post(carsController.editCarData);
 router.route("/delete-a-car").post(carsController.deleteACar);
